7dab1f23682d55d01e437e6401464d86b9f977fb
Tuukka <tuukka.tapani.r@gmail.com>
2020-12-02 19:47:27 +0200

    update to the nginx changes

--- webserver/nginx.nix
+++ webserver/nginx.nix
@@ -1,46 +1,46 @@
-{ config, pkgs, ... }: 
-{  
-security.acme.acceptTerms = true;
-security.acme.email = "admin+acme@example.com";
-
-services.nginx = {
-  enable = true;
-  virtualHosts = {
-    "cirno.world" = {
+{ config, pkgs, ... }:
+let
+  commonOptions =
+    {
       forceSSL = true;
       enableACME = true;
-      serverAliases = [ "www.cirno.world" ];
-      locations."/" = {
-        root = "/var/www/cirno";
-locations."~ \.php$".extraConfig = ''
-      fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-      fastcgi_index index.php;
-    '';
-};
+      locations."~ \.php$".extraConfig = ''
+        fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        fastcgi_index index.php;
+      '';
     };
-    security.acme.certs."cirno.world".extraDomainNames = [ "rodent.cirno.world" ];
-    "rodent.cirno.world" = {
-      forceSSL = true;
-      useACMEHost = "cirno.world";
-      locations."/" = {
-        root = "/var/www";
-locations."~ \.php$".extraConfig = ''
-      fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-      fastcgi_index index.php;
-    '';      
-};
+in
+{
+  security.acme.acceptTerms = true;
+  security.acme.email = "tuukka.t.korhonen@protonmail.com";
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "cirno.world" = (commonOptions // {
+        serverAliases = [ "www.cirno.world" ];
+        locations."/" = {
+          root = "/var/www/cirno";
+        };
+      });
+      security.acme.certs."cirno.world".extraDomainNames = [ "rodent.cirno.world" ];
+      "rodent.cirno.world" = (commonOptions // {
+        locations."/" = {
+          root = "/var/www/nazrin";
+        };
+      });
     };
   };
-services.phpfpm.pools.mypool = {                                                                                                                                                                                                             
-  user = "nobody";                                                                                                                                                                                                                           
-  settings = {                                                                                                                                                                                                                               
-    pm = "dynamic";            
-    "listen.owner" = config.services.nginx.user;                                                                                                                                                                                                              
-    "pm.max_children" = 5;                                                                                                                                                                                                                   
-    "pm.start_servers" = 2;                                                                                                                                                                                                                  
-    "pm.min_spare_servers" = 1;                                                                                                                                                                                                              
-    "pm.max_spare_servers" = 3;                                                                                                                                                                                                              
-    "pm.max_requests" = 500;                                                                                                                                                                                                                 
-  };        
+  services.phpfpm.pools.mypool = {
+    user = "nobody";
+    settings = {
+      pm = "dynamic";
+      "listen.owner" = config.services.nginx.user;
+      "pm.max_children" = 5;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 1;
+      "pm.max_spare_servers" = 3;
+      "pm.max_requests" = 500;
+    };
+  };
 }
-
