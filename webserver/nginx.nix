4bbfc0eb863c7d15e09c72e67f79e399ce35f530
Tuukka Korhonen <tuukka.korhonen@edu.sasky.fi>
2020-12-04 11:00:14 +0200

    again

--- webserver/nginx.nix
+++ webserver/nginx.nix
@@ -1,30 +1,35 @@
 { config, pkgs, ...}: 
 {
-services.nginx = {
-  enable = true;
-  virtualHosts."cirno.world" = {
-    enableACME = true;
-    forceSSL = true;
-    root = "/var/www/cirno";
-    locations."~ \.php$".extraConfig = ''
-      fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-      fastcgi_index index.php;
-    '';
+  security.acme.acceptTerms = true;
+  security.acme.email = "admin+acme@example.com";
+  
+  services.nginx = {
+    enable = true;
+    virtualHosts."cirno.world" = {
+      enableACME = true;
+      forceSSL = true;
+      root = "/var/www/cirno";
+      locations."~ \.php$".extraConfig = ''
+        fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+        fastcgi_index index.php;
+      '';
+    };
   };
-};
-services.mysql = {
-  enable = true;
-  package = pkgs.mariadb;
-};
-services.phpfpm.pools.mypool = {                                                                                                                                                                                                             
-  user = "nobody";                                                                                                                                                                                                                           
-  settings = {                                                                                                                                                                                                                               
-    pm = "dynamic";            
-    "listen.owner" = config.services.nginx.user;                                                                                                                                                                                                              
-    "pm.max_children" = 5;                                                                                                                                                                                                                   
-    "pm.start_servers" = 2;                                                                                                                                                                                                                  
-    "pm.min_spare_servers" = 1;                                                                                                                                                                                                              
-    "pm.max_spare_servers" = 3;                                                                                                                                                                                                              
-    "pm.max_requests" = 500;                                                                                                                                                                                                                 
-  };                                                                                                                                                                                                                                         
-};
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+  
+  services.phpfpm.pools.mypool = {
+    user = "nobody";
+    settings = {
+      pm = "dynamic";
+      "listen.owner" = config.services.nginx.user;
+      "pm.max_children" = 5;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 1;
+      "pm.max_spare_servers" = 3;
+      "pm.max_requests" = 500;
+    };
+  };
+}
