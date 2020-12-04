1272f5ff7cc2a4e9a29b143950054a423d4913d1
Tuukka Korhonen <tuukka.korhonen@edu.sasky.fi>
2020-12-04 10:55:21 +0200

    testing nginx with default config

--- webserver/nginx.nix
+++ webserver/nginx.nix
@@ -1,45 +1,30 @@
-{ config, pkgs, ... }:
-let
-  commonOptions =
-    {
-      forceSSL = true;
-      enableACME = true;
-      locations."~ \.php$".extraConfig = ''
-        fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
-        fastcgi_index index.php;
-      '';
-    };
-in
+{ config, pkgs, ...}: 
 {
-  security.acme.acceptTerms = true;
-  security.acme.email = "tuukka.t.korhonen@protonmail.com";
-  
-  services.nginx = {
-    enable = true;
-    virtualHosts = {
-      "cirno.world" = (commonOptions // {
-        serverAliases = [ "www.cirno.world" ];
-        locations."/" = {
-          root = "/var/www/cirno";
-        };
-      });    
-      "rodent.cirno.world" = (commonOptions // {
-        locations."/" = {
-          root = "/var/www/nazrin";
-        };
-      });
-    };
+services.nginx = {
+  enable = true;
+  virtualHosts."cirno.world" = {
+    enableACME = true;
+    forceSSL = true;
+    root = "/var/www/cirno";
+    locations."~ \.php$".extraConfig = ''
+      fastcgi_pass  unix:${config.services.phpfpm.pools.mypool.socket};
+      fastcgi_index index.php;
+    '';
   };
-  services.phpfpm.pools.mypool = {
-    user = "nobody";
-    settings = {
-      pm = "dynamic";
-      "listen.owner" = config.services.nginx.user;
-      "pm.max_children" = 5;
-      "pm.start_servers" = 2;
-      "pm.min_spare_servers" = 1;
-      "pm.max_spare_servers" = 3;
-      "pm.max_requests" = 500;
-    };
-  };
-}
+};
+services.mysql = {
+  enable = true;
+  package = pkgs.mariadb;
+};
+services.phpfpm.pools.mypool = {                                                                                                                                                                                                             
+  user = "nobody";                                                                                                                                                                                                                           
+  settings = {                                                                                                                                                                                                                               
+    pm = "dynamic";            
+    "listen.owner" = config.services.nginx.user;                                                                                                                                                                                                              
+    "pm.max_children" = 5;                                                                                                                                                                                                                   
+    "pm.start_servers" = 2;                                                                                                                                                                                                                  
+    "pm.min_spare_servers" = 1;                                                                                                                                                                                                              
+    "pm.max_spare_servers" = 3;                                                                                                                                                                                                              
+    "pm.max_requests" = 500;                                                                                                                                                                                                                 
+  };                                                                                                                                                                                                                                         
+};
