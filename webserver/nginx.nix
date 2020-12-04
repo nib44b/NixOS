bd43bbea52ed5f84e859c71375c8a0e0a1e2e638
Tuukka Korhonen <tuukka.korhonen@edu.sasky.fi>
2020-12-04 12:02:20 +0200

    test without php

--- webserver/nginx.nix
+++ webserver/nginx.nix
@@ -4,30 +4,12 @@
   security.acme.email = "tuukka.t.korhonen@protonmail.com";
   networking.firewall.allowedTCPPorts = [80 443];
   
-  services.nginx = {
-    enable = true;
-    virtualHosts."cirno.world" = {
-      enableACME = true;
-      forceSSL = true;
-      root = "/var/www/cirno";
-      locations."~ \.php$".extraConfig = ''
-        fastcgi_pass  unix:/run/phpfpm/cirno.socket;
-        fastcgi_index index.php;
-      '';
-    };
-  };
-
-  services.phpfpm.pools.cirno = {
-    user = "nginx";
-    settings = {
-      pm = "dynamic";
-      "listen.owner" = "nginx";
-      "pm.max_children" = 5;
-      "pm.start_servers" = 2;
-      "pm.min_spare_servers" = 1;
-      "pm.max_spare_servers" = 3;
-      "pm.max_requests" = 500;
-    };
-  };
+services.nginx.enable = true;
+services.nginx.virtualHosts."cirno.world" = {
+    addSSL = true;
+    forceSSL = true;
+    enableACME = true;
+    root = "/var/www/cirno";
+};
 }
 
