e547a330333a01fd583790cfa2a94e37346e4d01
Tuukka Korhonen <tuukka.korhonen@edu.sasky.fi>
2020-12-04 12:34:19 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- webserver/nginx.nix
+++ webserver/nginx.nix
@@ -4,11 +4,31 @@
   security.acme.email = "tuukka.t.korhonen@protonmail.com";
   networking.firewall.allowedTCPPorts = [80 443];
   
-services.nginx.enable = true;
-services.nginx.virtualHosts."cirno.world" = {
-    forceSSL = true;
+  services.nginx = {
+    enable = true;
+    virtualHosts."cirno.world" = {
     enableACME = true;
+    forceSSL = true;
     root = "/var/www/cirno";
-};
-}
+    locations."~ \.php$".extraConfig = ''
+      fastcgi_pass  unix:/run/phpfpm/cirno.sock;
+      fastcgi_index index.php;
+    '';
+    };
+  };
 
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+
+  services.phpfpm.pools.cirno = {                                                                                                                                                                                                             
+    user = "nginx";                                                                                                                                                                                                                           
+    settings = { 
+      "pm" = "dynamic";
+      "pm.max_children" = 75;
+      "pm.start_servers" = 10;
+      "pm.min_spare_servers" = 5;
+      "pm.max_spare_servers" = 20;
+      "pm.max_requests" = 500; }                                                                                                                                                                                                                                       
+  };
